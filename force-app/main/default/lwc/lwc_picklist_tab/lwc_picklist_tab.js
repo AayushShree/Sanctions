607cd5a1a9fc7fef11ef6c3a0677e86738b96532
Aayush Shree <aayushshree@gmail.com>
2024-10-27 13:11:06 +0530

    Created the lightning data table and resolved columns using results array field of json response to be diplsayed in lightning datatable

--- force-app/main/default/lwc/lwc_picklist_tab/lwc_picklist_tab.js
+++ force-app/main/default/lwc/lwc_picklist_tab/lwc_picklist_tab.js
@@ -5,6 +5,9 @@ import searchSanctions from '@salesforce/apex/SanctionsApiService.searchSanction
 
 export default class Lwc_picklist_tab extends LightningElement {
     selectedObject;
+    resultingarray=[];
+    countResult;
+    parsedData
     selectedField;
     fields = [];
     objectOptions = [];
@@ -12,6 +15,9 @@ export default class Lwc_picklist_tab extends LightningElement {
     name='';
     dataSurce='';
     searchResult;
+    tableData = [];
+    sortedBy;
+    sortedDirection;
 
     dataSourceOptions = [
             { label: 'United Nations', value: 'UN' },
@@ -22,6 +28,22 @@ export default class Lwc_picklist_tab extends LightningElement {
             { label: 'HM Treasury', value: 'HM TREASURY' },
             { label: 'OFAC-NONSDN', value: 'NONSDN' }
     ];
+
+    columns = [
+        //{label: 'S.No', fieldName: 'serialNumber', type: 'number', sortable:true},
+        {label: 'SI Indentifier', fieldName: 'si_Identifier', type:'text', sortable:true},
+        {label: 'Confidence Score', fieldName:'confidence_score', type:'text', sortable:true},
+        {label: 'Name', fieldName: 'name', type:'text', sortable:true},
+        {label: 'Alternate Names', fieldName: 'alt_names', type:'text', sortable:true},
+        {label: 'Entity Type', fieldName: 'entity_type', type:'text', sortable:true},
+        {label: 'Entity Number', fieldName:'entity_number', type:'number', sortable:true},
+        {label: 'Gender', fieldName:'gender', type:'text', sortable:true},
+        {label: 'Address', fieldName:'address', type:'text'},
+        {label: 'Data Source', fieldName: 'data_source', type:'text', sortable:true},
+        // {label: '', fieldName:'regime', type:'text', sortable:true, wrapText:true},
+        // {label: '', fieldName:'date_designated', type:'text', sortable:true, wrapText:true},
+        // {label: '', fieldName:'data_hash', type:'text', wrapText:true},
+    ]
 
     connectedCallback() {
         this.fetchObjectList();
@@ -122,7 +144,17 @@ export default class Lwc_picklist_tab extends LightningElement {
             })
                 .then(result => {
                     this.searchResult = result;
-                    this.prettyJsonResult = JSON.stringify(JSON.parse(result), null, 2);
+                    // this.prettyJsonResult = JSON.stringify(JSON.parse(result), null, 2);
+                    console.log('result in handle search', result);
+                    console.log('json parsed in searchclick', JSON.parse(result));
+
+                    //newcodeAG
+                    let parsedData = JSON.parse(result);  // Parse the JSON string
+                    console.log('Parsed JSON data:', parsedData);
+
+                    this.setData(parsedData);  // Set data for datatable
+                    //newcodeAGdone
+                    //this.setData(result);
                 })
                 .catch(error => {
                     this.searchResult = 'Error: ' + error.body.message;
@@ -132,5 +164,87 @@ export default class Lwc_picklist_tab extends LightningElement {
             this.searchResult = 'Please provide both name and at least one data source';
             this.prettyJsonResult = '';
         }
+    }
+
+    // setData(jsonData){
+    //     console.log('jsonData in set Data', jsonData);
+    //     this.resultingarray = jsonData.results;
+    //     console.log('resulting array', this.resultingarray);
+    //     try{
+    //         // this.tableData = jsonData;
+    //         //  console.log('resultingarray in try: ', this.resultingarray);
+    //         this.tableData = this.resultingarray.map((item) => {
+    //             return{
+    //                 // serialNumber: index + 1,
+    //                 // si_identifier: item.results.si_identifier || 'N/A',
+    //                 // entity_number: item.results.entity_number||'N/A',
+    //                 // position: item.results.additional_information && item.results.additional_information.position ? item.results.additional_information.position :'N/A',
+    //                 // gender: item.results.additional_information && item.results.additional_information.gender ? item.results.additional_information.gender :'N/A',
+    //                 // last_updated: item.results.additional_information && item.results.additional_information.last_updated ? item.results.additional_information.last_updated : 'N/A',
+    //                 // regime: item.results.additional_information && item.results.additional_information.regime ? item.results.additional_information.regime.join(', ') : 'N/A',
+    //                 // date_designated: item.results.additional_information && item.results.additional_information.date_designated ? item.results.additional_information.date_designated.join(', '): 'N/A',
+    //                 // data_hash: item.results.data_hash || 'N/A'                    
+    //                 //serialNumber: index+1,
+    //                 si_identifier: item.si_identifier,
+    //                 entity_number: item.entity_number,
+    //                 confidence_score: item.confidence_score,
+    //                 gender: item.gender,
+    //                 address: item.address,
+    //             };
+    //         });
+    //         console.log('this.tableData mapped', this.tableData);
+    //     }
+    //     catch(error){
+    //         console.error('Error parsing/mapping JSON data', error);
+    //         // this.tableData = [];
+    //         // this.sortedBy = null;
+    //         // this.sortedDirection = null;  // Reset sorting state when error occurs.  If you want to keep the current sorting state, comment out this line.  It will reset the sorting order on each error.  If you want to keep the previous sorting state, uncomment this line and comment out the previous line.  It will maintain the previous sorting order on each error.  You may need to update the sorting logic accordingly.  For example, if you want to keep the previous sorting order on each error, you can keep a copy of the original data and sort it on the client-side, then update the tableData with the sorted data.  If you want to reset the sorting order on each error, you can reset the tableData to the original data.  It depends on your specific use case.  You may need to make some adjust
+    //     }
+    // }
+
+    //newcodeAG
+    setData(jsonData) {
+        console.log('jsonData in set Data', jsonData);
+        this.resultingarray = jsonData.results;
+        console.log('resulting array', this.resultingarray);
+    
+        try {
+            this.tableData = this.resultingarray.map((item, index) => {
+                return {
+                    //serialNumber: index + 1,  
+                    si_Identifier: item.si_identifier || 'N/A',
+                    entity_number: item.entity_number || 'N/A',
+                    confidence_score: item.confidence_score || 'N/A',
+                    gender: item.gender || 'N/A',
+                    address: item.address ? item.address.join(', ') : 'N/A',
+                    name: item.name || 'N/A',  
+                    alt_names: item.alt_names ? item.alt_names.join(', ') : 'N/A',
+                    entity_type: item.entity_type || 'N/A',
+                    data_source: item.data_source ? item.data_source.name || 'N/A' : 'N/A',
+                };
+            });
+            console.log('this.tableData mapped', this.tableData);
+        } catch (error) {
+            console.error('Error parsing/mapping JSON data', error);
+        }
+    }
+
+
+    handleSort(event){
+        const { fieldName: sortedBy, sortedDirection} = event.detail;
+        const cloneData = [...this.tableData];
+
+        cloneData.sort((a, b) => {
+            let fieldA = a[sortedBy];
+            let fieldB = b[sortedBy];
+            if(typeof fieldA == 'string'){
+                fieldA = fieldA.toLowerCase();
+                fieldB = fieldB.toLowerCase();
+            }
+            return sortedDirection === 'asc' ? (fieldA>fieldB?1:-1) : (fieldA<fieldB?1:-1);
+        });
+        this.tableData = cloneData;
+        this.sortedBy = sortedBy;
+        this.sortedDirection = sortedDirection;
     }
 }
